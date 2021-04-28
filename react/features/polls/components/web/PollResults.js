1f8e1a6ae88d28b49500e44b1a91168e79a578f0
Fabien Zucchet <fabien.zucchet@student-cs.fr>
2021-04-28 10:11:46 +0200

    feat(Poll results): Use display name instead of ids in detailed results mode

--- react/features/polls/components/web/PollResults.js
+++ react/features/polls/components/web/PollResults.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-import { getParticipantDisplayName, getParticipants } from '../../../base/participants';
+import { getParticipantDisplayName } from '../../../base/participants';
 
 type Poll = {
 
