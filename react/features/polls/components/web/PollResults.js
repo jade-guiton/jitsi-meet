34b38bab0f90bc24c6bd616406abd03f02cc8e9e
spineki <marras.antoine@gmail.com>
2021-04-29 11:01:08 +0200

    feat(poll result) Added default message when trying to display no answer

--- react/features/polls/components/web/PollResults.js
+++ react/features/polls/components/web/PollResults.js
@@ -1,6 +1,7 @@
 // @flow
 
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 
 import { getParticipants } from '../../../base/participants';
@@ -38,22 +39,28 @@ function PollResults({ detailedVotes, displayQuestion, pollDetails }: Props) {
 
     const totalVoters = pollDetails.answers.reduce((accumulator, answer) => accumulator + answer.voters.size, 0);
 
+    const { t } = useTranslation();
+
+    // if no message for the moment, we stop here and display a warning message
+    if (totalVoters === 0) {
+        return <span> {t('poll.answer.empty')} </span>;
+    }
+
     const answers = pollDetails.answers.map((answer, index) => {
 
         const answerPercent = Math.round(answer.voters.size / totalVoters * 100);
 
-        const detailedAnswer =
-            detailedVotes
-            ? [ ...answer.voters ].map(voterId => {
-                const participant = participants.find(part => part.id === voterId);
-                console.log(participant);
+        const detailedAnswer
+            = detailedVotes
+                ? [ ...answer.voters ].map(voterId => {
+                    const participant = participants.find(part => part.id === voterId);
 
-                const name: string = participant ? participant.name : 'Fellow Jitser';
+                    const name: string = participant ? participant.name : 'Fellow Jitster';
 
-                return <li key = { voterId }>{ name }</li>;
-            })
+                    return <li key = { voterId }>{ name }</li>;
+                })
 
-            : null;
+                : null;
 
         return (
             <li key = { index }>
@@ -82,8 +89,6 @@ function PollResults({ detailedVotes, displayQuestion, pollDetails }: Props) {
         </div>
     );
 }
-
-            
 
 
 export default PollResults;
