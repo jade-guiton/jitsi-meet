874fa57fdd2fee046b3ff7b645ceadd6545d25d6
spineki <marras.antoine@gmail.com>
2021-04-29 13:28:48 +0200

    fix (polls) sanitizes imports to allow startup on react native

--- react/features/polls/components/web/PollCreateDialog.js
+++ react/features/polls/components/web/PollCreateDialog.js
@@ -7,7 +7,8 @@ import { Dialog } from '../../../base/dialog';
 import { translate } from '../../../base/i18n';
 import { Icon, IconAdd, IconClose } from '../../../base/icons';
 import { Tooltip } from '../../../base/tooltip';
-import { AbstractPollCreateDialog, AbstractProps } from '../AbstractPollCreateDialog';
+import { AbstractPollCreateDialog } from '../AbstractPollCreateDialog';
+import type { AbstractProps } from '../AbstractPollCreateDialog';
 
 type Props = AbstractProps & {
 
--- react/features/polls/components/web/index.js
+++ react/features/polls/components/web/index.js
@@ -2,3 +2,4 @@
 
 export { default as PollCreateButton } from './PollCreateButton';
 export { default as PollResults } from './PollResults';
+export { default as PollAnswerDialog} from './PollAnswerDialog'
