6a1c6cb0c0df081386153ac0a6f7bd98635a9fd3
Jade Guiton <jade.guiton@centralesupelec.fr>
2021-05-12 15:08:37 +0200

    fix(polls) Added a poll queue to avoid overwriting open modals

--- react/features/polls/actionTypes.js
+++ react/features/polls/actionTypes.js
@@ -13,6 +13,17 @@
 export const RECEIVE_POLL = 'RECEIVE_POLL';
 
 /**
+ * The type of the action which signals that a poll answer modal should be shown.
+ *
+ * {
+ *     type: SHOW_POLL,
+ *     pollId: string
+ * }
+ *
+ */
+export const SHOW_POLL = 'SHOW_POLL';
+
+/**
  * The type of the action which signals that a new Answer was received.
  *
  * {
