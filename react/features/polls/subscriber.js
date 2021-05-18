59251f1ae13b50711ae5d8f6b97e3ed6c5c1b26e
Jade Guiton <jade.guiton@centralesupelec.fr>
2021-05-18 18:21:45 +0200

    feat(polls) Using sendMessage instead of sendCommandOnce, switched poll IDs to string, and ability to receive old polls from backend

--- react/features/polls/subscriber.js
+++ react/features/polls/subscriber.js
@@ -11,46 +11,62 @@ StateListenerRegistry.register(
     state => getCurrentConference(state),
     (conference, store, previousConference) => {
         if (conference && conference !== previousConference) {
+            conference.room.addListener('xmmp.json_message_received', (senderJid, data) => {
+                if(data.type === COMMAND_NEW_POLL) {
+                    const { question, answers, pollId, senderId } = data;
+                    
+                    const poll = {
+                        senderId,
+                        answered: false,
+                        question,
+                        answers: answers.map(answer => {
+                            return {
+                                name: answer,
+                                voters: new Map()
+                            };
+                        })
+                    };
 
-            // Command triggered when a new poll is received
-            conference.addCommandListener(COMMAND_NEW_POLL, ({ attributes, children }) => {
-                const poll = {
-                    senderId: attributes.senderId,
-                    answered: false,
-                    question: attributes.question,
-                    answers: children.map(answerData => {
-                        return {
-                            name: answerData.value,
-                            voters: new Map()
+                    const dialogComponent = store.getState()['features/base/dialog'].component;
+                    const queue = dialogComponent !== undefined;
+
+                    store.dispatch(receivePoll(pollId, poll, queue));
+                    
+                } else if(data.type === COMMAND_ANSWER_POLL) {
+                    const { pollId, answers, senderId, voterName } = data;
+
+                    const receivedAnswer: Answer = {
+                        senderId,
+                        voterName,
+                        pollId,
+                        answers,
+                    };
+
+                    store.dispatch(receiveAnswer(pollId, receivedAnswer));
+                    
+                } else if(data.type === 'old-polls') {
+                    const { polls } = data;
+                    
+                    for(const pollId in polls) {
+                        const { senderId, question, answers } = polls[pollId];
+                        const poll = {
+                            senderId,
+                            answered: true,
+                            question,
+                            answers: answers.map(answer => {
+                                const voters = new Map();
+                                for(const voterId in answer.voters) {
+                                    voters.set(voterId, answer.voters[voterId]);
+                                }
+                                return {
+                                    name: answer.name,
+                                    voters
+                                };
+                            })
                         };
-                    })
-                };
-
-                const dialogComponent = store.getState()['features/base/dialog'].component;
-                const queue = dialogComponent !== undefined;
-                const pollId = parseInt(attributes.pollId, 10);
-
-                store.dispatch(receivePoll(pollId, poll, queue));
-            });
-
-            // Command triggered when new answer is received
-            conference.addCommandListener(COMMAND_ANSWER_POLL, ({ attributes, children }) => {
-                const { dispatch } = store;
-                const { senderId, voterName, pollId } = attributes;
-
-                const receivedAnswer: Answer = {
-                    senderId,
-                    voterName,
-                    pollId: parseInt(pollId, 10),
-                    answers: children.map(
-
-                            // Boolean are converted to text through XMPP
-                            // We convert here the strings back to boolean
-                            element => element.attributes.checked === 'true'
-                    )
-                };
-
-                dispatch(receiveAnswer(pollId, receivedAnswer));
+                        store.dispatch(receivePoll(pollId, poll, false));
+                    }
+                }
             });
         }
     }
