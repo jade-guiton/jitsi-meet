c08817c68718a7869f80d6076bfee8c829e48e25
Fabien Zucchet <fabien.zucchet@student-cs.fr>
2021-05-21 16:26:23 +0200

    fix(poll.vote count): Fix votes counting when computing percentage

--- react/features/polls/components/AbstractPollResults.js
+++ react/features/polls/components/AbstractPollResults.js
@@ -55,10 +55,13 @@ const AbstractPollResults = (Component: AbstractComponent<AbstractProps>) => (pr
     const answers: Array<AnswerInfo> = useMemo(() => {
         const voterSet = new Set();
 
+        console.log(pollDetails.answers);
+
         // Getting every voters ID that participates to the poll
         for (const answer of pollDetails.answers) {
-            for (const voter of answer.voters) {
-                voterSet.add(voter);
+            for (const [ voterId ] of answer.voters) {
+                console.log(voterId);
+                voterSet.add(voterId);
             }
         }
 
